d9541a799d00b55b9eff9481f6ea10c2ce84dde1
lazehang <lazehang@gmail.com>
2018-08-09 12:55:40 +0800

    sd

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
+ const path = require('path');
 const app = express();
 const hb = require('express-handlebars');
 const basicAuth = require('express-basic-auth');
